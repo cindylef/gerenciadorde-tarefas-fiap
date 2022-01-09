9387eff91628dce15fa0df20b94e894c9170714e
Cindy Lou Eng da Fonseca <95055598+cindylef@users.noreply.github.com>
2022-01-09 21:37:16 -0200

    Commit

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import moment from "moment";
+import { NextPage } from "next";
+type FooterProps = {
+    showModal() : void
+}
+
+export const Footer : NextPage<FooterProps> = ({ showModal}) => {
+
+    return (
+
+        <div className="container-footer">
+
+            <button onClick={showModal}><img src="/add.svg" alt="Adicionar Tarefa"/> Adiciona uma tarefa</button>
+           
+            <span>© Copyright {moment().year()}. Todos os direitos reservados.</span>
+
+        </div>
+    );
+}
---
+++ containers/Login.tsx
@@ -0,0 +1,87 @@
+import { NextPage } from "next";
+import { useState } from "react"
+import { executeRequest } from "../services/api";
+import { LoginRequest } from "../types/LoginRequest";
+import { LoginResponse } from "../types/LoginResponse";
+
+type LoginProps = {
+    setToken(s: string) : void
+}
+
+export const Login : NextPage<LoginProps> = ({setToken}) => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [msgError, setError] = useState('');
+
+    const doLogin = async () => {
+
+        try {
+
+            if (!login || !password) {
+
+                setError('Favor preencher os dados');
+
+                return;
+            }
+
+            setError('');
+
+            const body = {
+
+                login,
+                password
+            };
+
+            const result = await executeRequest('login', 'POST', body);
+
+            if(result && result.data){
+
+                const loginResponse = result.data as LoginResponse;
+                localStorage.setItem('accessToken', loginResponse.token);
+                localStorage.setItem('userName', loginResponse.name);
+                localStorage.setItem('userEmail', loginResponse.email);
+                setToken(loginResponse.token);
+            }
+        } catch (e : any) {
+
+            if(e?.response?.data?.error){
+                console.log(e?.response);
+                setError(e?.response?.data?.error);
+                return;
+            }
+            console.log(e);
+            setError('Ocorreu um erro ao efetuar o login, tente novamente');
+        }
+    }
+
+    return (
+
+        <div className="container-login">
+
+            <img src="/logo.svg" alt="Logo Fiap" className="logo" />
+
+            <div className="form">
+                
+                {msgError && <p>{msgError}</p>}
+                <div className="input">
+                    <img src="/mail.svg" alt="Informe o seu email" />
+                    <input type="text" placeholder="Informe o seu email"
+                        value={login} onChange={evento => setLogin(evento.target.value)} />
+                </div>
+
+                <div className="input">
+
+                    <img src="/lock.svg" alt="Informe a sua senha" />
+                    <input type="password" placeholder="Informe a sua senha"
+                        value={password} onChange={evento => setPassword(evento.target.value)} />
+
+                </div>
+
+                <button onClick={doLogin}>Login</button>
+
+            </div>
+
+        </div>
+    )
+}
